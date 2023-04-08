e76e66f06cad5c6e290be71a88c07167edabbc13
myRepository <appireddypeketi@gmail.com>
2023-04-08 06:45:53 +0530

    add test1.java file

--- src/test/java/org/qa/gitCommands/Test1.java
+++ src/test/java/org/qa/gitCommands/Test1.java
@@ -3,7 +3,7 @@ package org.qa.gitCommands;
 public class Test1 {
 
 	public static void main(String[] args) {
-	System.out.println("It is for my code");
+	System.out.println("It is for my code reddy");
 	
 
 	}
