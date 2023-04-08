39fe63a4f6fa68a289cd0706d9fd623c92701cb2
appireddy-peketi <54722437+appireddy-peketi@users.noreply.github.com>
2023-04-09 00:05:49 +0530

    Update Test1.java

--- src/test/java/Tests/Test1.java
+++ src/test/java/Tests/Test1.java
@@ -3,7 +3,7 @@ package Tests;
 public class Test1 {
 
 	public static void main(String[] args) {
-	System.out.println("It is for my code reddy");
+	System.out.println("it code is from git"):
 	
 
 	}
